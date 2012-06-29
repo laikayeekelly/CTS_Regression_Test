9fa31a2373375004611b72babab519891a12afe6
laikayeekelly <laikayeekelly@yahoo.com.hk>
2012-06-29 17:57:04 +0800

    version1 changed

--- regression_version1.py
+++ regression_version1.py
@@ -9,29 +9,20 @@ output.write('<TestPlan version="1.0">\n')
 line = ''
 package = ''
 xml_text = ''
-package_list = []
-start_a_new_package = 1
-no_of_failed_test_cases = 0       
+start_a_new_package = True  
 
-while line.find('</TestResult>') is -1 :
-    if line.find('<TestPackage') != -1:
-        tmp = line.split('"')  
-        package = tmp[3]                         
-        start_a_new_package = 1                  
+while not('</TestResult>' in line):
+    if ('<TestPackage' in line):
+        package = line.split('"')[3]
+        start_a_new_package = True                 
         
-    if (line.find('result="fail"') != -1): 
-        no_of_failed_test_cases = no_of_failed_test_cases + 1
-        if (start_a_new_package == 1):
-            start_a_new_package = 0
-            package_list.append(package)              
+    if ('result="fail"' in line): 
+        if (start_a_new_package == True):
+            start_a_new_package = False            
             xml_text = '  <Entry uri="' + package + '"/>\n'
             output.write(xml_text)
                 
     line = input.readline()
-
-if (no_of_failed_test_cases >= 1000) : 
-    print 'Number of failed test cases exceeds 1000 '
-    print 'regression testing is not suggested'
 
 output.write('</TestPlan>\n')
 input.close()
