3fa78814aacd5b06602320979576c2f373e13c5f
laikayeekelly <laikayeekelly@yahoo.com.hk>
2012-07-04 10:55:01 +0800

    version 2 code using another library called ElementTree

---
+++ regression_version2_ElementTree.py
@@ -0,0 +1,18 @@
+from xml.etree.ElementTree import ElementTree
+tree = ElementTree()
+tree.parse("testResult.xml")
+package = tree.findall("TestPackage")
+
+output = open('ctsRegression.xml', 'w')
+output.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+output.write('<TestPlan version="1.0">\n')
+
+for i in package:
+	testcases = list(i.iter("Test"))
+	for j in testcases:
+		if(j.attrib["result"]=='fail'):
+			output.write('  <Entry uri="' + i.attrib["appPackageName"] + '"/>\n')
+			break
+               
+output.write('</TestPlan>\n')
+output.close()
