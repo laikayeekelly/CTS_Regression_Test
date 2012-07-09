14cec1811ef0234701bbaba60a4894789dda5451
laikayeekelly <laikayeekelly@yahoo.com.hk>
2012-07-09 16:38:10 +0800

    ConsolidatedReport.py changed with adding the line for outputing the process of the program

--- ConsolidatedReport.py
+++ ConsolidatedReport.py
@@ -15,6 +15,7 @@ if os.path.exists(sys.argv[1]) == False:
 file_list = ReportLib.list_files(sys.argv[1])
 for each_file in file_list:
     failcase = ReportLib.find_fail_case(each_file, failcase)
+    print "Finished processing file " + each_file
 ReportLib.write_to_output(sys.argv[2], failcase, len(file_list))
 
 print "Finished!!!"
