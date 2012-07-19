b49b2fe3acf170728b50757b4bebeb30649524f6
laikayeekelly <laikayeekelly@yahoo.com.hk>
2012-07-19 17:59:04 +0800

    regression_test.py added

---
+++ regression_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import os
+import sys
+import subprocess
+import regressionLib
+
+#subprocess.call( ["cts-tradefed"])
+#subprocess.call( ["run", "cts", "--plan", "CTS"])
+
+regression_done = 0
+fail = regressionLib.generate_regression_plan()
+
+while (regression_done != 10) and (fail):
+    print 'run regression test'
+    #subprocess.call( ["cts-tradefed"])
+    #subprocess.call( ["run", "cts", "--plan", "regressionCTS"])
+    fail = regressionLib.generate_regression_plan()
+    regression_done += 1
+
+regressionLib.generate_consolidated_report(sys.argv[1])
+
+print "Finished!!!"
+
