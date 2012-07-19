f17f7a2b8d189500b7e73c10644a25ca8da43c47
laikayeekelly <laikayeekelly@yahoo.com.hk>
2012-07-19 18:01:37 +0800

    regression_test.py updated

--- regression_test.py
+++ regression_test.py
@@ -3,10 +3,11 @@ import os
 import sys
 import subprocess
 import regressionLib
+from time import sleep
 
 #subprocess.call( ["cts-tradefed"])
 #subprocess.call( ["run", "cts", "--plan", "CTS"])
-
+#sleep(5)
 regression_done = 0
 fail = regressionLib.generate_regression_plan()
 
@@ -14,6 +15,7 @@ while (regression_done != 10) and (fail):
     print 'run regression test'
     #subprocess.call( ["cts-tradefed"])
     #subprocess.call( ["run", "cts", "--plan", "regressionCTS"])
+    #sleep(5)
     fail = regressionLib.generate_regression_plan()
     regression_done += 1
 
