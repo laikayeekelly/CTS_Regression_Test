e34b66393c69237381fac6704907caf6bb985c9f
laikayeekelly <laikayeekelly@yahoo.com.hk>
2012-07-20 14:07:59 +0800

    regression_test.py changed

--- regression_test.py
+++ regression_test.py
@@ -7,16 +7,17 @@ from time import sleep
 
 #subprocess.call( ["cts-tradefed"])
 #subprocess.call( ["run", "cts", "--plan", "CTS"])
-#sleep(2)
+sleep(1)
 regression_done = 0
 fail = regressionLib.generate_regression_plan()
-#sleep(2)
-while (regression_done != 5) and (fail):
+sleep(1)
+while (regression_done != 10) and (fail):
     print 'run regression test\n'
     #subprocess.call( ["cts-tradefed"])
     #subprocess.call( ["run", "cts", "--plan", "regressionCTS"])
-    #sleep(2)
+    sleep(1)
     fail = regressionLib.generate_regression_plan()
+    sleep(1)
     regression_done += 1
 
 regressionLib.generate_consolidated_report(sys.argv[1])
