ba45479d3e75388037a769e0c122c8a3078d2e9e
laikayeekelly <laikayeekelly@yahoo.com.hk>
2012-07-23 11:10:15 +0800

    regression_test.py changed with the changing on the number of regression test to be done is not restricted but become an input argument

--- regression_test.py
+++ regression_test.py
@@ -9,23 +9,25 @@ from time import sleep
 
 report_file_list = []
 
-if len(sys.argv) != 2:
-    raise NameError("usage: regression.py output_csv")
+
+if len(sys.argv) != 3:
+    raise NameError("usage: regression.py no_of_regression output_csv")
 
 #subprocess.call( ["cts-tradefed"])
 #subprocess.call( ["run", "cts", "--plan", "CTS"])
 
+no_of_regression = int(sys.argv[1])
 regression_done = 0
 (report_file_list, fail) = regressionLib.generate_regression_plan(report_file_list)
 
-while (regression_done != 10) and (fail):
+while (regression_done != no_of_regression) and (fail):
     print 'run regression test\n'
     #subprocess.call( ["cts-tradefed"])
     #subprocess.call( ["run", "cts", "--plan", "regressionCTS"])
     (report_file_list, fail) = regressionLib.generate_regression_plan(report_file_list)
     regression_done += 1
 
-regressionLib.generate_consolidated_report(sys.argv[1], report_file_list)
+regressionLib.generate_consolidated_report(sys.argv[2], report_file_list)
 
 print "Finished!!!"
 
