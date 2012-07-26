62f2a9b22f4fef4f48f76c32e487f1d3e81b68a2
laikayeekelly <laikayeekelly@yahoo.com.hk>
2012-07-26 10:59:26 +0800

    regression_test.py changed

--- regression_test.py
+++ regression_test.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
-import regressionLib
+import ctsutil
 from time import sleep
+from ctsutil import run_test, generate_regression_plan, generate_consolidated_report
+
 
 # This executable python file should be put in the folder android-cts/tools
 
@@ -14,12 +16,16 @@ report_file_list = []
 regression_done = 0
 no_of_regression = int(sys.argv[1])
 
-report_file_list, fail = regressionLib.run_test("CTS", report_file_list)
+run_test()
+report_file, fail = generate_regression_plan()
+report_file_list.append(report_file)
 while (regression_done != no_of_regression) and (fail):
-    print 'run regression test\n'
-    report_file_list, fail = regressionLib.run_test("regressionCTS", report_file_list) 
+    #print 'run regression test\n'
+    run_test("regressionCTS") 
+    report_file, fail = generate_regression_plan()
+    report_file_list.append(report_file)
     regression_done += 1
 
-regressionLib.generate_consolidated_report(sys.argv[2], report_file_list)
+generate_consolidated_report(sys.argv[2], report_file_list)
 
 print "Finished!!!"
