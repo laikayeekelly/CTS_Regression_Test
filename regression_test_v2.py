04a2e166a08864769879a292a92953feb3945063
laikayeekelly <laikayeekelly@yahoo.com.hk>
2012-07-26 16:41:45 +0800

    version2 of ctsutil.py and regression_test.py

---
+++ regression_test_v2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import os
+import sys
+import ctsutil
+from time import sleep
+from ctsutil import run_test, generate_regression_plan, generate_consolidated_report
+
+
+# This executable python file should be put in the folder android-cts/tools
+
+if len(sys.argv) != 3:
+    print "usage: regression.py no_of_regression output_csv"
+    exit()
+
+report_file_list = []
+regression_done = 0
+no_of_regression = int(sys.argv[1])
+
+report_file = run_test()
+report_file_list.append(report_file)
+fail = generate_regression_plan(report_file)
+while (regression_done != no_of_regression) and (fail):
+    print 'Finished generating regression test plan and start running regression test\n'
+    report_file = run_test("regressionCTS") 
+    report_file_list.append(report_file)
+    fail = generate_regression_plan(report_file)
+    regression_done += 1
+
+generate_consolidated_report(sys.argv[2], report_file_list)
+
+print "Finished!!!"
