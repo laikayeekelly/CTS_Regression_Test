128f3ec5062df82b4ad102d0a0653d2d9e71de33
laikayeekelly <laikayeekelly@yahoo.com.hk>
2012-08-01 15:13:03 +0800

    regression_test.py changed

--- regression_test.py
+++ regression_test.py
@@ -3,29 +3,29 @@ import os
 import sys
 import ctsutil
 from time import sleep
-from ctsutil import run_test, cts_report_filter, generate_consolidated_report
+from ctsutil import run_test, cts_report_filter, consolidate_report
 
 
 # This executable python file should be put in the folder android-cts/tools
 
 if len(sys.argv) != 3:
-    print "usage: regression.py no_of_regression output_csv"
+    print "usage: regression.py max_regression output_csv"
     exit()
 
-report_file_list = []
-regression_done = 0
-no_of_regression = int(sys.argv[1])
+report_list = []
+regression_count = 0
+max_regression = int(sys.argv[1])
 
-report_file = run_test()
-report_file_list.append(report_file)
-test_plan = cts_report_filter(report_file)
-while (regression_done != no_of_regression) and (test_plan != ''):
+report = run_test()
+report_list.append(report)
+plan = cts_report_filter(report)
+while (regression_count < max_regression) and (plan != None):
     print 'Finished generating regression test plan and start running regression test\n'
-    report_file = run_test(test_plan) 
-    report_file_list.append(report_file)
-    test_plan = cts_report_filter(report_file)
-    regression_done += 1
+    report = run_test(plan) 
+    report_list.append(report)
+    plan = cts_report_filter(report)
+    regression_count += 1
 
-generate_consolidated_report(sys.argv[2], report_file_list)
+consolidate_report(report_list, sys.argv[2])
 
 print "Finished!!!"
