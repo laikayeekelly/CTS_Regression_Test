6b05a111addf735564da9866d225232919c7620a
laikayeekelly <laikayeekelly@yahoo.com.hk>
2012-08-10 17:14:27 +0800

    regression_test.py changed

--- regression_test.py
+++ regression_test.py
@@ -3,7 +3,7 @@ import os
 import sys
 import ctsutil
 from time import sleep
-from ctsutil import run_test, cts_report_filter, consolidate_report
+from ctsutil import run_test, cts_report_filter, consolidate_report, check_adb_connection
 
 
 # This executable python file should be put in the folder android-cts/tools
@@ -14,16 +14,23 @@ if len(sys.argv) != 3:
 
 report_list = []
 regression_count = 0
-max_regression = int(sys.argv[1])
+max_regression = int(sys.argv[1])-1
 
-report = run_test()
+report, complete_execute = run_test()
 report_list.append(report)
 plan = cts_report_filter(report)
+if complete_execute == False:
+    check_adb_connection()
+    exit()
+
 while (regression_count < max_regression) and (plan != None):
-    report = run_test(plan) 
+    report, complete_execute = run_test(plan) 
     report_list.append(report)
     plan = cts_report_filter(report)
     regression_count += 1
+    if complete_execute == False:
+        check_adb_connection()
+        exit()
 
 consolidate_report(report_list, sys.argv[2])
 
